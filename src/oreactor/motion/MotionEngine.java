fba913386562ccd2eb380af7b807e37d1f712f32
dakusui <dakusui@77cbd7d9-8f8c-4ec4-8850-4144602c1108>
2011-06-04 15:00:20 +0000

    git-svn-id: https://svn.osdn.net/svnroot/aviator1945/branches/OpenReactor@100 77cbd7d9-8f8c-4ec4-8850-4144602c1108

--- src/oreactor/motion/MotionEngine.java
+++ src/oreactor/motion/MotionEngine.java
@@ -1,16 +1,27 @@
 package oreactor.motion;
 
+import oreactor.core.BaseEngine;
+import oreactor.core.Settings;
 import oreactor.motion.MotionProvider.InteractionMode;
 
-public class MotionEngine {
+public class MotionEngine extends BaseEngine {
 	MotionProvider provider = null;
+
+	public MotionEngine(Settings settings, MotionProvider motionProvider) {
+		super(settings);
+		this.provider = motionProvider;
+		
+	}
+
 	public void run() {
-		provider.prepareActions();
-		provider.performScavenging();
-		provider.performActions();
-		provider.performInteractions(InteractionMode.Collision);
-		provider.performInteractions(InteractionMode.Generic);
-		provider.commit();
-		provider.reset();
+		if (provider != null) {
+			provider.prepareActions();
+			provider.performScavenging();
+			provider.performActions();
+			provider.performInteractions(InteractionMode.Collision);
+			provider.performInteractions(InteractionMode.Generic);
+			provider.commit();
+			provider.reset();
+		}
 	}
 }
