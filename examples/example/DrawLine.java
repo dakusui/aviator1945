c8ee0ba9dc7049d0cece280803c372d79c6b4569
dakusui <dakusui@77cbd7d9-8f8c-4ec4-8850-4144602c1108>
2011-06-16 17:43:39 +0000

    git-svn-id: https://svn.osdn.net/svnroot/aviator1945/branches/OpenReactor@152 77cbd7d9-8f8c-4ec4-8850-4144602c1108

--- examples/example/DrawLine.java
+++ examples/example/DrawLine.java
@@ -1,8 +1,6 @@
 package example;
 
-import mu64.Mu64Reactor;
-
-public class DrawLine extends Mu64Reactor {
+public class DrawLine extends mu64.Mu64Reactor {
 	public void run() throws ExitMu64 {
 		graphicsplane().clear();
 		graphicsplane().line(0, 0, 1023, 767, Pallette.WHITE);
